cf2c7a3d51fc724fc13cfd43fa64d88c590e15cd
Miguel445Ar <86012555+Miguel445Ar@users.noreply.github.com>
2022-11-14 21:06:31 -0500

    first commit

---
+++ PruebaTecnica-Backend/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PruebaTecnica_Backend
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
